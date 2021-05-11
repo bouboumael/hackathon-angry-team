7bcaf58105d9f1e01bc4b26097ec667100601263
RedPandore <tennessee-houry_student2021@wilder.school>
2021-05-11 14:39:24 +0200

    db

--- db.sql
+++ db.sql
@@ -1,3 +1,15 @@
+CREATE TABLE `planete` (
+  `id` int PRIMARY KEY,
+  `name` varchar(255) NOT NULL,
+  `localisation_id` int
+);
+
+CREATE TABLE `localisation` (
+  `id` int PRIMARY KEY,
+  `restaurant_id` int NOT NULL,
+  `planete_id` int NOT NULL
+);
+
 CREATE TABLE `restaurant` (
   `id` int PRIMARY KEY,
   `name` varchar(255) NOT NULL,
@@ -33,3 +45,7 @@ ALTER TABLE `menu` ADD FOREIGN KEY (`plat_id`) REFERENCES `plat` (`id`);
 ALTER TABLE `menu` ADD FOREIGN KEY (`boisson_id`) REFERENCES `boisson` (`id`);
 
 ALTER TABLE `restaurant` ADD FOREIGN KEY (`menu_id`) REFERENCES `menu` (`id`);
+
+ALTER TABLE `localisation` ADD FOREIGN KEY (`planete_id`) REFERENCES `planete` (`id`);
+
+ALTER TABLE `localisation` ADD FOREIGN KEY (`restaurant_id`) REFERENCES `restaurant` (`id`);
