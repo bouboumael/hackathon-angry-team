31545f93059d5715745371af2667af5b29018d6e
RedPandore <tennessee-houry_student2021@wilder.school>
2021-05-12 10:04:34 +0200

    resto

--- src/Controller/RestaurantController.php
+++ src/Controller/RestaurantController.php
@@ -3,8 +3,6 @@
 namespace App\Controller;
 
 use App\Controller\AbstractController;
-use App\Model\BoissonManager;
-use App\Model\PlatManager;
 use App\Model\RestaurantManager;
 
 class RestaurantController extends AbstractController
@@ -12,17 +10,13 @@ class RestaurantController extends AbstractController
     public function index($id)
     {
         $restaurantManager = new RestaurantManager();
-        $boissonManager = new BoissonManager();
-        $platManager = new PlatManager();
-        $platLists = $platManager->selectAll();
-        $boissonLists = $boissonManager->selectAll();
+        $boissonLists = $restaurantManager->selectWithDrink();
         $restaurants = $restaurantManager->selectByIdCategory($id);
         $restaurantName = $restaurantManager->selectNameById($id);
 
         return $this->twig->render('Restaurant/index.html.twig', [
             'restaurants' => $restaurants,
             'restaurantName' => $restaurantName,
-            'platLists' => $platLists,
             'boissonLists' => $boissonLists,
         ]);
     }
