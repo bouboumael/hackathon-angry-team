d31509f8217c9ae6d4e2ff5167e66a1d882b2036
Valentin <valentin.lay98@gmail.com>
2021-05-12 08:08:27 +0200

    securisation from

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -6,34 +6,57 @@ use App\Controller\AbstractController;
 
 class ContactController extends AbstractController
 {
-    private const MAX_LENGHT = 255;
-
-    public function index()
-    {
-        return $this->twig->render('Contact/index.html.twig');
-    }
+    private const NAME_MAX_LENGTH = 80;
+    private const MESSAGE_MAX_LENGTH = 2000;
 
     public function validate($data)
     {
         $errors = [];
-        $data = array_map('trim', $_POST);
 
-        if (empty($data['name'])) {
-            $errors[] = 'Le nom du restaurant est obligatoire';
-        } elseif (strlen($data['name']) > self::MAX_LENGHT) {
-            $errors[] = 'Le nom doit faire moin de ' . self::MAX_LENGHT . ' caractères.';
+        if (empty($data['lastname'])) {
+            $errors[] = 'Le nom est obligatoire';
+        } elseif ($data['lastname'] > self::NAME_MAX_LENGTH) {
+            $errors = 'Le nom doit faire moins de' . self::NAME_MAX_LENGTH . ' caractères';
         }
 
-        if (empty($data['localisation'])) {
-            $errors[] = 'La location est obligatoire';
-        } elseif (strlen($data['localisation']) > self::MAX_LENGHT) {
-            $errors[] = 'la location doit faire moin de ' . self::MAX_LENGHT . ' caractères.';
+        if (empty($data['firstname'])) {
+            $errors[] = 'Le prénom est obligatoire';
+        } elseif ($data['firstname'] > self::NAME_MAX_LENGTH) {
+            $errors = 'Le prénom doit faire moins de' . self::NAME_MAX_LENGTH . ' caractères';
         }
 
-        if (empty($data['description'])) {
-            $errors[] = 'La description est obligatoire';
+        if (empty($data['telephone'])) {
+            $errors[] = 'Un numéro de téléphone est obligatoire';
         }
 
+        if (empty($data['message'])) {
+            $errors[] = 'Un message est obligatoire';
+        } elseif ($data['message'] > self::MESSAGE_MAX_LENGTH) {
+            $errors = 'Le message doit faire moins de ' . self::MESSAGE_MAX_LENGTH . ' caractères';
+        }
         return $errors;
+    }
+
+    private function send()
+    {
+        $errors = [];
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $data = array_map('trim', $_POST);
+            $errors = $this->validate($data);
+
+            if (empty($errors)) {
+                //send in mail
+                header('location: /Contact/index');
+            }
+        }
+        return $errors;
+    }
+
+    public function index()
+    {
+        $errors = $this->send();
+        return $this->twig->render('Contact/index.html.twig', [
+            "errors" => $errors
+        ]);
     }
 }
